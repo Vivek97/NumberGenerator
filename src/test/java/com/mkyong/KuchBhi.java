fe675953b251fe839e92b114b0205d9beb0d6898
Vivek Jain <jainvivek.jain01@gmail.com>
2018-05-24 17:37:58 +0530

    All files

---
+++ src/test/java/com/mkyong/KuchBhi.java
@@ -0,0 +1,71 @@
+package com.mkyong;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class KuchBhi {
+
+	@Test
+	public void testLengthOfTheUniqueKey() {
+
+	//	System.out.println("slkhngklsdglgksdnlnd");
+		App obj = new App();
+		 
+		Assert.assertEquals(36, 6*6);
+
+		System.out.println("called            ");
+		
+		System.out.println("called            1");
+		System.out.println("called            2");
+		System.out.println("called            3");
+	}
+	
+	
+
+	@Test
+	public void testLengthOfTheUniqueKey1() {
+
+	//	System.out.println("slkhngklsdglgksdnlnd");
+		App obj = new App();
+		 
+		Assert.assertEquals(36, obj.generateUniqueKey().length());
+
+		System.out.println("called            ");
+		
+		System.out.println("called            1");
+		System.out.println("called            2");
+		System.out.println("called            3");
+	}
+	
+
+	@Test
+	public void LengthOfTheUniqueKey() {
+
+	//	System.out.println("slkhngklsdglgksdnlnd");
+		App obj = new App();
+		 
+		Assert.assertEquals(36, obj.generateUniqueKey().length());
+
+		System.out.println("called            ");
+		
+		System.out.println("called            1");
+		System.out.println("called            2");
+		System.out.println("called            3");
+	}
+	
+
+	@Test
+	public void testLengthOfTheUnique() {
+
+	//	System.out.println("slkhngklsdglgksdnlnd");
+		App obj = new App();
+		 
+		Assert.assertEquals(36, obj.generateUniqueKey().length());
+
+		System.out.println("called            ");
+		
+		System.out.println("called            1");
+		System.out.println("called            2");
+		System.out.println("called            3");
+	}
+}
